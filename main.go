06a0d886286242536bc8bb5a3228ac45badc84fc
karokojnr <karoko.jnr99@gmail.com>
2024-01-02 14:35:49 +0300

    Handlers boilerplate setup :rocket:

--- main.go
+++ main.go
@@ -29,15 +29,22 @@ func main() {
 		MaxAge:           300,
 	}))
 
+	v1Router := chi.NewRouter()
+	v1Router.Get("/healthz", handlerRediness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	srv := &http.Server{
 		Handler: router,
 		Addr:    ":" + port,
 	}
+
+	log.Printf("Server starting on port %v", port)
 
 	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	log.Printf("Server starting on port %v", port)
 }
